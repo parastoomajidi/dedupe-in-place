d1bc32a187fb975ec263b2760b16d1faae510d32
Parastoo Majidi <61692804+parastoomajidi@users.noreply.github.com>
2025-11-22 15:20:59 +0000

    add solution in general way

---
+++ solution.py
@@ -0,0 +1,14 @@
+class solution
+    def find k(self, nums):
+        k = 0
+        iw = 0
+        ir = 0
+        
+        while(ir < len(nums)):
+            if nums(ir) != val
+                k += 1
+                num(iw) = num[ir]
+                iw += 1
+            ir += 1
+
+        return K
