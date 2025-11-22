56d41e4c35037db42b98f1874ef4aca05c9df909
Parastoo Majidi <61692804+parastoomajidi@users.noreply.github.com>
2025-11-22 15:30:54 +0000

    fix buges

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 class solution
-    def find k(self, nums):
+    def find k(self, nums, val):
         k = 0
         iw = 0
         ir = 0
         
         while(ir < len(nums)):
-            if nums(ir) != val
+            if nums[ir] != val:
                 k += 1
-                num(iw) = num[ir]
+                nums[iw] = nums[ir]
                 iw += 1
             ir += 1
 
-        return K
+        return k
